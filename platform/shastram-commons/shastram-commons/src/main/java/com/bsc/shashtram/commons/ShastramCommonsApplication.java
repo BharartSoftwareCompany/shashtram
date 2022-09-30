9b24592cecc44c6b2aa2573753610c84981a0f2e
mahalelabs <mahale.labs@gmail.com>
2022-09-30 16:05:42 +0530

    m2

---
+++ platform/shastram-commons/shastram-commons/src/main/java/com/bsc/shashtram/commons/ShastramCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.bsc.shashtram.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShastramCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShastramCommonsApplication.class, args);
+	}
+
+}
---
+++ platform/shastram-services/shastram-services/src/test/java/com/bsc/shashtram/services/ShastramServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsc.shashtram.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShastramServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
