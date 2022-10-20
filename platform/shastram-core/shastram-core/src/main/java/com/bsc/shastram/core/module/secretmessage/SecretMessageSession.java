6b5f7cb4d17ae43946ac611ba078fbd828c576bd
MGM <MGM@10.22.108.220>
2022-10-20 15:16:47 +0530

    m2

--- platform/shastram-core/shastram-core/src/main/java/com/bsc/shastram/core/module/secretmessage/SecretMessageSession.java
+++ platform/shastram-core/shastram-core/src/main/java/com/bsc/shastram/core/module/secretmessage/SecretMessageSession.java
@@ -5,10 +5,25 @@ import javax.persistence.Table;
 
 import com.bsc.shastram.commons.base.BaseIDEntity;
 
+import lombok.Data;
+
 
 @Table( schema= "secretmessage", name ="secret_message_session")
 @Entity
+@Data
 public class SecretMessageSession extends BaseIDEntity {
+	
+	private static final long serialVersionUID = 1L;
+	private boolean isCreatedByRegisteredUser;
+	private String userId;
+	private String tile;
+	private String description;
+	private long expiry;
+	private boolean isExpired = false;
+	private String shortCode;
+	private boolean isPublic = true;
+	
+	
 	
 
 }
