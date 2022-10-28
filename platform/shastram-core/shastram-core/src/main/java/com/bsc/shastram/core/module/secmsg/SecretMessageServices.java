6a51f039d804ab8e53d0eedfcd43ca1fbc976ae3
YMGM <mahale.labs@gmail.com>
2022-10-28 15:38:55 +0530

    rearranged

--- platform/shastram-core/shastram-core/src/main/java/com/bsc/shastram/core/module/secmsg/SecretMessageServices.java
+++ platform/shastram-core/shastram-core/src/main/java/com/bsc/shastram/core/module/secmsg/SecretMessageServices.java
@@ -1,9 +1,13 @@
+/*********************************************
+ *  @author Madhusudhan Mahale |  2022 
+ *********************************************/
 package com.bsc.shastram.core.module.secmsg;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bsc.shastram.core.model.Context;
+import com.bsc.shastram.core.module.secmsg.model.SecretMessage;
 import com.bsc.shastram.core.module.secmsg.model.SecretMessageSession;
 import com.bsc.shastram.core.module.secmsg.repo.SecretMessageRepo;
 import com.bsc.shastram.core.module.secmsg.repo.SecretMessageSessionRepo;
@@ -23,10 +27,19 @@ public class SecretMessageServices {
 	
 	
 	
-	
+	//Session related services
 	public SecretMessageSession createSession(SecretMessageSession session,Context context)
 	{
+		session = secMessageSessionRepo.save(session);
 		return session;
+	}
+	
+	
+	//Message related Services
+	public SecretMessage saveMessage(SecretMessage message,Context context)
+	{
+		message = secretMessageRepo.save(message);
+		return message;
 		
 	}
 	
---
+++ platform/shastram-core/shastram-core/src/main/java/com/bsc/shastram/core/module/secmsg/SecretMessageSessionController.java
@@ -0,0 +1,12 @@
+package com.bsc.shastram.core.module.secmsg;
+
+import org.springframework.stereotype.Controller;
+import com.bsc.shastram.core.controller.BaseController;
+
+
+@Controller
+public class SecretMessageSessionController extends BaseController {
+	
+	
+
+}
